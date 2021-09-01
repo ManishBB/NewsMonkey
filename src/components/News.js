66b89573618efce6366e4585c3531519ca4f8742
Manish Bhamare <mbb7224@gmail.com>
2021-09-01 16:15:41 +0530

    Added pagination to limit articles.

--- src/components/News.js
+++ src/components/News.js
@@ -181,17 +181,46 @@ export class News extends Component {
         console.log("From news item!")
         this.state = {
             articles : this.articles,
-            loading : false
+            loading : false,
+            page : 1
         }
     }
 
     async componentDidMount(){
-        console.log("cdl")
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=87925e2b98644535b4cd74a092bd9900";
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=87925e2b98644535b4cd74a092bd9900&page=1&pageSize=20";
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData)
-        this.setState({articles : parsedData.articles})
+        this.setState({articles : parsedData.articles, totalResults : parsedData.totalResults})
+    }
+
+    handlePrevClick = async ()=>{
+
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=87925e2b98644535b4cd74a092bd9900&page=${this.state.page - 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+
+      this.setState({
+        page : this.state.page - 1,
+        articles : parsedData.articles
+      })
+    }
+    handleNextClick = async ()=>{
+
+      if(this.state.page+1 > Math.ceil(this.state.totalResults/20)){
+
+      }
+      else{
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=87925e2b98644535b4cd74a092bd9900&page=${this.state.page + 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+
+        this.setState({
+          page : this.state.page + 1,
+          articles : parsedData.articles
+        })
+      }
+      
     }
 
     render() {
@@ -205,6 +234,10 @@ export class News extends Component {
                         <NewsItem title={element.title?element.title.slice(0,40):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
                     </div>
                     })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr;Previous</button>
+                <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next&rarr;</button>
                 </div>
                 
             </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,12 +7,12 @@ export class NewsItem extends Component {
  
         return (
             <div>
-                <div className="card" style={{width: '18rem'}}>
+                <div className="card my-2" style={{width: '18rem'}}>
                 <img src={!imageUrl?"https://cdn.wionews.com/sites/default/files/styles/story_page/public/2021/04/08/190606-untitled-design-11.jpg":imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}...</h5>
                     <p className="card-text">{description}...</p>
-                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-primary btn-sm">Read more</a>
+                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-dark btn-sm">Read more</a>
                 </div>
                 </div>
             </div>
