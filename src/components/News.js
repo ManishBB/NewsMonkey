2313ae2ab5a7c8914bb3ed6eacded45e5fd0163c
Manish Bhamare <mbb7224@gmail.com>
2021-09-02 11:23:26 +0530

    Added top loading bar

--- src/components/News.js
+++ src/components/News.js
@@ -36,13 +36,16 @@ export class News extends Component {
     }
 
     async updateNews(){
+      this.props.setProgress(10)
       const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=87925e2b98644535b4cd74a092bd9900&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
+        this.props.setProgress(45)
         let parsedData = await data.json();
-        console.log(parsedData)
+        this.props.setProgress(75)
         this.setState({articles : parsedData.articles, 
           totalResults : parsedData.totalResults, 
           loading:false})
+        this.props.setProgress(100)
     }
 
     async componentDidMount(){
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,8 +8,15 @@ export class NewsItem extends Component {
         return (
             <div>
                 <div className="card my-2">
-                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left : '90%', zIndex : '1'}}>{source}
-                </span>
+                <div style={{
+                    display : 'flex',
+                    justifyContent : 'flex-end',
+                    position : 'absolute',
+                    right : 0
+                }}>
+                <span className="badge rounded-pill bg-danger">{source}</span>
+                </div>
+                
                 <img src={!imageUrl?"https://cdn.wionews.com/sites/default/files/styles/story_page/public/2021/04/08/190606-untitled-design-11.jpg":imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}...</h5>
