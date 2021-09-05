447ce1406ee3319d3f326fdff070c534a75f1371
Manish Bhamare <mbb7224@gmail.com>
2021-09-05 14:07:43 +0530

    Removed buttons Next & Previous

--- src/components/News.js
+++ src/components/News.js
@@ -37,38 +37,6 @@ const News = (props) => {
       // eslint-disable-next-line
     }, [])
 
-    const handlePrevClick = async ()=>{
-
-      // let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page - 1}&pageSize=${props.pageSize}`;
-      // this.setState({loading : true})
-      // let data = await fetch(url);
-      // let parsedData = await data.json();
-
-      // this.setState({
-      //   page : page - 1,
-      //   articles : parsedData.articles,
-      //   loading : false
-      // })
-
-      setPage(page-1)
-      updateNews();
-    }
-
-    const handleNextClick = async ()=>{
-        // let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`;
-        // this.setState({loading : true})
-        // let data = await fetch(url);
-        // let parsedData = await data.json();
-
-        // this.setState({
-        //   page : page + 1,
-        //   articles : parsedData.articles,
-        //   loading : false
-        // })
-        setPage(page+1)
-        updateNews();
-    }
-
     const fetchMoreData = async() => {
       const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
       setPage(page+1)
@@ -104,8 +72,6 @@ const News = (props) => {
                 </div>
                 </InfiniteScroll>
                 <div className="container d-flex justify-content-between">
-                <button disabled={page<=1} type="button" className="btn btn-dark" onClick={handlePrevClick}>&larr;Previous</button>
-                <button disabled={page+1 > Math.ceil(totalResults/props.pageSize)} type="button" className="btn btn-dark" onClick={handleNextClick}>Next&rarr;</button>
                 </div>
             </>
         )
